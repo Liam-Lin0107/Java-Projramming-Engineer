b4e308efc0b040859ac68aa7fe0f001e2f5baeda
Liam-Lin0107 <geffc1454@gmail.com>
2022-09-14 14:56:25 +0800

    finish the first course

--- Image Filter/action.js
+++ Image Filter/action.js
@@ -1,7 +1,7 @@
 var inputImg = null, canvas;
 
 function loadImage() {
-    inputImg = document.getElementById("inputImg").value;
+    inputImg = document.getElementById("inputImg");
     var img = new SimpleImage(inputImg);
     canvas = document.getElementById("can1");
     img.drawTo(canvas);
